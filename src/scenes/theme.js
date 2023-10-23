592942b57b3eef3c59ec4603585631762ffb90bb
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2023-10-22 18:04:52 -0700

    More setips and
    Add scene and .jsx files and try to work the theme with tailwind css.

---
+++ src/scenes/theme.js
@@ -0,0 +1,6 @@
+/*Here are the setups for the colors and the types of website */
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material";
+import { indigo } from "@mui/material/colors";
+
+//color design tokens
